d884f39a2f4bbe03ce1e305f75f17dd02bfbfc2a
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-10 19:56:34 +0300

    customized admin list view

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,6 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-    list_filter = ('title','author','publication_year')
+    search_fields = ('title','author','publication_year')
 
 admin.site.register(Book)
