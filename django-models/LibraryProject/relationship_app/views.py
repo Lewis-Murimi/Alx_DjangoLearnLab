ea5f39247ddf88735422ace83cc8d0954bc4d6ea
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-18 17:47:58 +0300

    fixed views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,5 +13,5 @@ class LibraryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.filter(library=self.object)
+        context['books'] = Book.objects.all()
         return context
