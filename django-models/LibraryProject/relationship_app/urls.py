5b43f3fdec251c27403c92744ba43e163e3a740d
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-18 18:11:47 +0300

    authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,14 @@
+from tempfile import template
+
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .models import Library, Book, Librarian
 from django.views.generic.detail import DetailView
 
@@ -15,3 +20,16 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.all()
         return context
+
+class Register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+    def get_form(self, form_class=None):
+        return UserCreationForm()
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
