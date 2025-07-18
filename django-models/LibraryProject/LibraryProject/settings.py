1534d963477ec52b52bc3f5fcd0eed2174cb2e38
Lewis-Murimi <lewismurimi195@gmail.com>
2025-07-18 17:41:17 +0300

    views

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf.apps.BookshelfConfig',
+    'relationship_app',  # Ensure this app is included
 ]
 
 MIDDLEWARE = [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Author, Book, Library, Librarian
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    pk_url_kwarg = 'pk'
