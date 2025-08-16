46f6420e024c1d49511c86cc422397c6cb5df748
Lewis-Murimi <lewismurimi195@gmail.com>
2025-08-16 23:10:06 +0300

    fixed urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="posts"),                 # matches your navbar
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-edit"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
